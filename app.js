52e17efa696011d94ff8e8adc01b73fd451cc9ca
yumimm <yuu9916@gmail.com>
2021-11-21 01:53:26 +0800

    Initialize Project

---
+++ app.js
@@ -0,0 +1,83 @@
+// target the input element
+const input = document.getElementById('action__input_1')
+// click handler
+const toggleMenu = e => {
+  // target the menu element
+  const menu = document.getElementById('action__menu_1')
+  menu.classList.toggle('hidden')
+}
+// bind the event
+input.addEventListener('click', toggleMenu)
+
+// ------------------ dark mode-----------------------
+// target the switch element
+const darkModeToggle = document.getElementById('dark__mode__toggle')
+// toggle handler
+const darkModeToggleHandler = e => {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark')
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light')
+  }
+}
+
+// bind the event
+darkModeToggle.addEventListener('change', darkModeToggleHandler)
+
+// //--------------- markup effect-------------------
+// // 選取點擊範圍
+const BOX = document.querySelector('.checkbox')
+const ROW = BOX.parentElement.parentElement
+const checkBoxChecked = e => {
+  // 被點到後會addClass
+  if (BOX.checked) {
+    ROW.classList.add('table__row--markup')
+  } else {
+    ROW.classList.remove('table__row--markup')
+  }
+}
+
+BOX.addEventListener('click', checkBoxChecked)
+
+
+
+// 原本的CODE,只會執行一次就就不會再次執行
+
+// --------------- markup effect-------------------
+// 設立選取範圍
+const BOX = document.querySelectorAll('input[type="checkbox"]')
+const select = document.querySelector('.checkall')
+const allRow = document.querySelectorAll('.table__row')
+
+function selectRow () {
+  for (let i = 2; i < BOX.length; i++) {
+    BOX[i].onclick = function () {
+      if (BOX[i].checked) { 
+        this.parentElement.parentElement.classList.add('table__row--markup')
+      } else {
+        this.parentElement.parentElement.classList.remove('table__row--markup')
+      }
+    }
+  }
+}
+
+function SelectAll () {
+  if (select.checked == true) {
+    for (let i = 1; i < BOX.length; i++) {
+      BOX[i].checked = true
+      for (let j = 0; j < allRow.length; j++) {
+        allRow[j].classList.add('table__row--markup')
+      }
+    }
+  } else if (select.checked == false) {
+    for (let i = 1; i < BOX.length; i++) {
+      BOX[i].checked = false
+      for (let j = 0; j < allRow.length; j++) {
+        allRow[j].classList.remove('table__row--markup')
+      }
+    }
+  }
+}
+
+select.addEventListener('click', SelectAll)
+selectRow()
